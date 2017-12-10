ae2e5a63db2aa201c021ea9b6ed5705e0a9d180b
buzzcola <orr.james@gmail.com>
2017-12-09 21:27:09 -0500

    basic thing

--- docs/program.js
+++ docs/program.js
@@ -1,31 +1,28 @@
-namespace threeNumbers {
-    let $: Function = document.querySelector.bind(document); // this is just less typing.
-    let input1: HTMLInputElement;
-    let input2: HTMLInputElement;
-    let input3: HTMLInputElement;
-    let result: HTMLElement;
-
+var threeNumbers;
+(function (threeNumbers) {
+    let $ = document.querySelector.bind(document); // this is just less typing.
+    let input1;
+    let input2;
+    let input3;
+    let result;
     function init() {
         input1 = $('#input1');
         input2 = $('#input2');
         input3 = $('#input3');
         result = $('#result');
-
         [input1, input2, input3].forEach(i => {
             i.addEventListener('change', getResult);
             i.value = '0';
         });
         getResult();
     }
-
-    function getResult(): void {
+    function getResult() {
         let value = calculate(+input1.value, +input2.value, +input3.value);
         result.innerText = '' + value;
     }
-
-    function calculate(v1: number, v2: number, v3: number): number {
+    function calculate(v1, v2, v3) {
         return v1 + v2 + v3;
     }
-
     document.addEventListener("DOMContentLoaded", init);
-}
+})(threeNumbers || (threeNumbers = {}));
+//# sourceMappingURL=program.js.map
